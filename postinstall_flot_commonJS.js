3c4f24befefa2faad966ff16995ada29d86b225b
nkasprak <nkasprak@gmail.com>
2018-06-30 14:14:46 -0400

    this ought to work

--- postinstall_flot_commonJS.js
+++ postinstall_flot_commonJS.js
@@ -1,24 +1,21 @@
 try {
   var fs = require("fs");
-  var handleDirectory = function(files) {
+  var handleDirectory = function(files, address) {
     files.forEach(function(file) {
       if (file.indexOf("jquery.flot")!==-1) {
-        var js = fs.readFileSync("./node_modules/jquery-flot/" + file);
-        fs.writeFileSync("./" + file.replace(".js","") + "_commonJS.js", "module.exports = function(jQuery) {" + js + "};");
+        var fileAddress = address + "/" + file;
+        var js = fs.readFileSync(fileAddress,"utf-8");
+        var dest = "./" + file.replace(".js","") + "_commonJS.js";
+        fs.writeFileSync(dest, "module.exports = function(jQuery) {" + js + "};");
       }
     });
   };
-  if (fs.existsSync("./node_modules/jquery-flot")) {
-    console.log("node modules in this folder");
-    var files = fs.readdirSync("./node_modules/jquery-flot");
-    handleDirectory(files);
-  }
-  if (fs.existsSync("../jquery-flot")) {
-    
-    console.log("flot in sibling folder");
-    var files = fs.readdirSync("../jquery-flot");
-    handleDirectory(files);
-  }
+  ["../jquery-flot","./node_modules/jquery-flot"].forEach(address=> {
+    if (fs.existsSync(address)) {
+      var files = fs.readdirSync(address);
+      handleDirectory(files, address);
+    }
+  });
 } catch (ex) {
   console.log(ex);
 }
