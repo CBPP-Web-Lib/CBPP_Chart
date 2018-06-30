9d06bb13e235cb8cf17a1655b0d11b0810b3557c
nkasprak <nkasprak@gmail.com>
2018-06-30 14:02:37 -0400

    debugging changes

--- postinstall_flot_commonJS.js
+++ postinstall_flot_commonJS.js
@@ -9,10 +9,13 @@ var handleDirectory = function(files) {
   });
 };
 if (fs.existsSync("./node_modules/jquery-flot")) {
+  console.log("node modules in this folder");
   var files = fs.readdirSync("./node_modules/jquery-flot");
   handleDirectory(files);
 }
 if (fs.existsSync("../jquery-flot")) {
+  
+  console.log("flot in sibling folder");
   var files = fs.readdirSync("../jquery-flot");
   handleDirectory(files);
 }
