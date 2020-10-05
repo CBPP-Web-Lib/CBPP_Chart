d7aa9c08da4499d21acc8fa5bc92944d90111131
Nick Kasprak <nkasprak@gmail.com>
2020-10-05 12:43:39 -0400

    use __dirname to find flot

--- postinstall_flot_commonJS.js
+++ postinstall_flot_commonJS.js
@@ -5,15 +5,15 @@ try {
       if (file.indexOf("jquery.flot")!==-1) {
         var fileAddress = address + "/" + file;
         var js = fs.readFileSync(fileAddress,"utf-8");
-        var dest = "./" + file.replace(".js","") + "_commonJS.js";
+        var dest = __dirname + "/" + file.replace(".js","") + "_commonJS.js";
         fs.writeFileSync(dest, "module.exports = function(jQuery) {" + js + "};");
       }
     });
   };
   ["../jquery-flot","./node_modules/jquery-flot"].forEach(address=> {
-    if (fs.existsSync(address)) {
-      var files = fs.readdirSync(address);
-      handleDirectory(files, address);
+    if (fs.existsSync(__dirname + "/" + address)) {
+      var files = fs.readdirSync(__dirname + "/" + address);
+      handleDirectory(files, __dirname + "/" + address);
     }
   });
 } catch (ex) {
